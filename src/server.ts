5faeb8af08b7d4499d62da2c066fe30ac0d6e412
DESKTOP-HVU60H0\Bruno Coutinho <bscoutinho2012@gmail.com>
2021-03-07 12:33:54 +0000

    aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database/index.ts'
+import { router } from './routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(router)
 
 
 app.get("/", (request, response) => {
