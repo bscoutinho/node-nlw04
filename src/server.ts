00a3200307e0cd5f3715c64fbf68089d72b87519
DESKTOP-HVU60H0\Bruno Coutinho <bscoutinho2012@gmail.com>
2021-03-16 20:10:33 +0000

    Project Finished

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,3 @@
-import 'reflect-metadata'
-import express from 'express'
-import './database/index.ts'
-import { router } from './routes'
+import { app } from "./app";
 
-const app = express()
-
-app.use(express.json())
-app.use(router)
-
-
-app.get("/", (request, response) => {
-    return response.send({ message: 'Teste MSG'})
-})
-
-app.post("/", (request, response) => {
-    return response.json({ message: 'Dados salvos com sucesso'})
-})
-
-app.listen(3333, () => console.log('Server is running'))
+app.listen(3333, () => console.log("Server is running!"));
